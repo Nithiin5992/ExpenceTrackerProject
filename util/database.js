43da80257dadf821d07cd4bcf29e2577b9c43128
Nithiin5992 <nithinkumar2513@gmail.com>
2023-08-28 23:21:12 +0530

    database name correction

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('expencetracker','Nithinkumar','Nithin5992',{
+const sequelize = new Sequelize('Expencetracker','Nithinkumar','Nithin5992',{
   dialect: 'mysql',
   host: 'database-1.ci588dctszbs.us-east-1.rds.amazonaws.com'
 });
